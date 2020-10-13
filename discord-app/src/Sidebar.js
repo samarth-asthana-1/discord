be9d316ddeee11a3cdd4a5fe6e5528159809a14c
Samarth <samarthasthana110@gmail.com>
2020-10-14 02:01:37 +0530

    made some changes in sidebar

--- discord-app/src/Sidebar.js
+++ discord-app/src/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel.js";
 
 function Sidebar() {
     return (
@@ -10,13 +12,19 @@ function Sidebar() {
                 <ExpandMoreIcon/>
             </div>
             <div className="sidebar__channels">
-                <div className="sidebar__Channel">
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon />
+                        <h4>Text Channel</h4>
+                    </div>
 
+                    <AddIcon className="sidebar__addChannel"/>
                 </div>
-
+            </div>
+            <div className="sidebar__channelList">
+                <SidebarChannel />
             </div>
         </div>
     )
 }
-
 export default Sidebar
---
+++ discord-app/src/SidebarChannel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarChannel() {
+    return (
+        <div className="sidebarChannel">
+            <h4><span className="sidebarChannel__hash">#</span>Discord</h4>
+        </div>
+    )
+}
+
+export default SidebarChannel
