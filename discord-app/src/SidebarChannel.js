e44137b392218ab304ed4808ce7689f55ea566f3
Samarth <samarthasthana110@gmail.com>
2020-10-14 15:12:44 +0530

    added sidebar bottom

--- discord-app/src/SidebarChannel.js
+++ discord-app/src/SidebarChannel.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import './SidebarChannel.css';
 
-function SidebarChannel() {
-    return (
-        <div className="sidebarChannel">
-            <h4><span className="sidebarChannel__hash">#</span>Discord</h4>
-        </div>
-    )
+function SidebarChannel({id,channel}) {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannel__hash">#</span>Discord
+      </h4>
+    </div>
+  );
 }
 
-export default SidebarChannel
+export default SidebarChannel;
