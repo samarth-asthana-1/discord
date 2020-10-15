f7e58473431d0eaff4c64003d0267459803d895c
Samarth <samarthasthana110@gmail.com>
2020-10-15 14:50:50 +0530

    almost done

--- discord-app/src/Login.js
+++ discord-app/src/Login.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import './Login.css';
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "./firebase";
 
 function Login() {
-    return (
-        <div>
-            <h2>Welcome</h2>
-        </div>
-    )
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img
+          alt=""
+          src="https://yt3.ggpht.com/a/AATXAJw1tXvJInOnm44MdEF3kS0b8x-W4Twj27SYvJ9gRw=s900-c-k-c0xffffffff-no-rj-mo"
+        />
+      </div>
+      <Button onClick={signIn}>SignIn</Button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
